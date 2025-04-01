8b7dc0699090229226a3cc54186ec6c0aec56d3c
musapeace <mubarakinda@gmail.com>
2025-04-01 03:37:09 +0100

    refactor models: add name field to Bus and restructure Line and Load models

--- loadflow_api/loadflow/models.py
+++ loadflow_api/loadflow/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class Bus(models.Model):
+    name = models.CharField(max_length=100)
     voltage = models.FloatField()
-    angle = models.FloatField()
 
-class Generator(models.Model):
-    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
-    power_output = models.FloatField()
+class Line(models.Model):
+    from_bus = models.ForeignKey(Bus, related_name='from_lines', on_delete=models.CASCADE)
+    to_bus = models.ForeignKey(Bus, related_name='to_lines', on_delete=models.CASCADE)
+    impedance = models.FloatField()
 
 class Load(models.Model):
-    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
-    power_demand = models.FloatField()
+    bus = models.ForeignKey(Bus, related_name='loads', on_delete=models.CASCADE)
+    power = models.FloatField()
